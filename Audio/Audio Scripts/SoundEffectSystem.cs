062e380aa5b4c68035f43b13c52732edbc00a904
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2020-06-02 17:20:58 -0400

    Add files via upload

---
+++ Audio/Audio Scripts/SoundEffectSystem.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEffectSystem : MonoBehaviour
+{
+
+    public AudioSource oneOrTwo;
+    public AudioSource q;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            oneOrTwo.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            oneOrTwo.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            q.Play();
+        }
+    }
+}
